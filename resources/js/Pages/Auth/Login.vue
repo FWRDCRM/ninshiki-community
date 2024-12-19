0efdd03cd25d1e37a922c4a3f5c1cbdd9adb5547
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-19 17:32:24 +0800

    ✨ feat(Auth): Implement Zoho login button on the login page. ➕ refactor(package.json): Add `primeicons` as a project dependency.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -4,9 +4,16 @@
 <template>
     <div class="container flex min-h-dvh min-w-full">
         <div class="flex items-center justify-center w-full">
-            <p>Continue with Zoho</p>
+            <div class="flex gap-3 flex-col">
+                <p class="font-bold text-3xl text-gray-600">Login</p>
+                <p class="font-normal">Enter your credentials to access your Ninshiki account.</p>
+                <Button class="mt-8" label="Continue with Zoho">
+                    <template #icon>
+                        <img alt="zohoIcon" src="https://api.iconify.design/logos:zoho.svg">
+                    </template>
+                </Button>
+            </div>
         </div>
-
     </div>
 </template>
 
