7f1b86c31ea19ec5e8274b90f84521fb27959cc3
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-21 14:12:02 +0800

    ✨ feat(Pages/Feeds.vue): Display user information on the feed page.

--- resources/js/Pages/Feeds.vue
+++ resources/js/Pages/Feeds.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { computed } from 'vue'
+import { usePage } from '@inertiajs/vue3'
 
+
+
+const page = usePage()
+const user = computed(() => page.props.auth.user)
 </script>
 
 <template>
-
+<div class="text-red-500">
+    {{ JSON.stringify(user) }}
+</div>
 </template>
 
 <style scoped>
