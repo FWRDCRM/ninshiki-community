549931f2be349e9449ffb6013d04d92c0228f82c
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-21 19:29:07 +0800

    WIP

--- resources/js/Inertia/Pages/feed/index.vue
+++ resources/js/Inertia/Pages/feed/index.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { computed } from 'vue'
 import { usePage } from '@inertiajs/vue3'
-
+import Layout from '../../Layouts/layout.vue'
 
 const page = usePage()
 const user = computed(() => page.props.auth.user)
 defineProps({ posts: Object })
+defineOptions({ layout: Layout })
 </script>
 
 <template>
