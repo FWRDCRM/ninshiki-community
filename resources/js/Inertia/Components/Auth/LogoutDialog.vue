a246c2fa929a80a45465d57df7a4dd4b1286fcd7
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-22 12:34:29 +0800

    ✨ feat(Components/Auth/LogoutDialog.vue): Add logout dialog component.

---
+++ resources/js/Inertia/Components/Auth/LogoutDialog.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ConfirmDialog from 'primevue/confirmdialog';
+import {usePage} from "@inertiajs/vue3";
+
+const page = usePage()
+</script>
+
+<template>
+    <ConfirmDialog group="headless">
+        <template #container="{ message, acceptCallback, rejectCallback }">
+            <div class="flex flex-col items-center p-8 bg-surface-0 dark:bg-surface-900 rounded">
+                <Avatar
+                    :image="page.props.auth.user.avatar ?? `https://avatar.iran.liara.run/username?username=${page.props.auth.user.email}`"
+                    class="mr-2 rounded-full  h-24 w-24 -mt-20 inline-flex" shape="circle" size="xlarge"/>
+                <span class="font-bold text-2xl block mb-2 mt-6">{{ message.header }}</span>
+                <p class="mb-0">{{ message.message }}</p>
+                <div class="flex items-center gap-2 mt-6">
+                    <Button label="Cancel" severity="secondary" @click="rejectCallback"></Button>
+                    <Button label="Logout" severity="danger" @click="acceptCallback"></Button>
+                </div>
+            </div>
+        </template>
+    </ConfirmDialog>
+</template>
+
+<style scoped>
+
+</style>
