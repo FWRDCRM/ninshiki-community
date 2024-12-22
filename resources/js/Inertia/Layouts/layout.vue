b17cc6c7b94cac120ba82462810407a0b461bee6
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-22 12:00:59 +0800

    ✨ feat(Layouts/layout.vue): Improve sidebar stickiness and navigation logic.

--- resources/js/Inertia/Layouts/layout.vue
+++ resources/js/Inertia/Layouts/layout.vue
@@ -17,9 +17,9 @@ const items = ref([
         icon: 'pi pi-home',
         shortcut: '⌘+F',
         command: () => {
-          router.visit(route('feed'), {
-            only: ['posts'],
-          })
+            route().current('feed') ?
+                router.reload({ only: ['posts'] }) : router.push(route('feed'));
+
         }
       },
       {
@@ -38,49 +38,46 @@ const items = ref([
 <template>
   <div class="flex">
     <main class="flex px-28 justify-center pt-10 ">
-      <div class="h-screen sticky top-10">
+      <div class="h-screen">
         <!-- Fixed Sidebar -->
-        <div class="card flex justify-center">
-          <Menu :model="items" class="w-full md:w-60">
-            <template #start>
+          <Menu :model="items" class="w-full md:w-60 sticky top-10">
+              <template #start>
                 <span class="inline-flex items-center gap-1 px-2 py-2">
                     <span class="text-xl font-semibold text-primary">
                       {{ page.props.app.name }}
                     </span>
                 </span>
-            </template>
-            <template #submenulabel="{ item }">
-              <span class="text-primary font-bold">{{ item.label }}</span>
-            </template>
-            <template #item="{ item, props }">
-              <a v-ripple class="flex items-center" v-bind="props.action">
-                <span :class="item.icon"/>
-                <span>{{ item.label }}</span>
-                <Badge v-if="item.badge" class="ml-auto" :value="item.badge"/>
-                <span v-if="item.shortcut"
-                      class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{
-                    item.shortcut
-                  }}</span>
-              </a>
-            </template>
-            <template #end>
-              <button v-ripple
-                      class="relative overflow-hidden w-full border-0 bg-transparent flex items-start p-2 pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
-                <Avatar
-                    :image="page.props.auth.user.avatar ?? `https://avatar.iran.liara.run/username?username=${page.props.auth.user.email}`"
-                    class="mr-2" shape="circle"/>
-                <span class="inline-flex flex-col items-start">
+              </template>
+              <template #submenulabel="{ item }">
+                  <span class="text-primary font-bold">{{ item.label }}</span>
+              </template>
+              <template #item="{ item, props }">
+                  <a v-ripple class="flex items-center" v-bind="props.action">
+                      <span :class="item.icon"/>
+                      <span>{{ item.label }}</span>
+                      <Badge v-if="item.badge" class="ml-auto" :value="item.badge"/>
+                      <span v-if="item.shortcut"
+                            class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{
+                              item.shortcut
+                          }}</span>
+                  </a>
+              </template>
+              <template #end>
+                  <button v-ripple
+                          class="relative overflow-hidden w-full border-0 bg-transparent flex items-start p-2 pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
+                      <Avatar
+                          :image="page.props.auth.user.avatar ?? `https://avatar.iran.liara.run/username?username=${page.props.auth.user.email}`"
+                          class="mr-2" shape="circle"/>
+                      <span class="inline-flex flex-col items-start">
                         <span class="text-balance font-bold">{{ page.props.auth.user.name }}</span>
                         <span class="text-xs font-normal italic text-gray-400">{{ page.props.auth.user.email }}</span>
                 </span>
-              </button>
-              <p class="flex flex-row-reverse italic w-full p-2 font-normal text-sm text-gray-300">
-                {{ page.props.app.version }}
-              </p>
-            </template>
+                  </button>
+                  <p class="flex flex-row-reverse italic w-full p-2 font-normal text-sm text-gray-300">
+                      {{ page.props.app.version }}
+                  </p>
+              </template>
           </Menu>
-
-        </div>
       </div>
 
       <div>
