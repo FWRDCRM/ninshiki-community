ba4b8cddf4a7c04640e048f954d3450f3848e457
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-22 12:13:42 +0800

    ✨ feat(`Inertia/Pages/feed/index.vue`): Update import path for Layout component.

--- resources/js/Inertia/Pages/feed/index.vue
+++ resources/js/Inertia/Pages/feed/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed } from 'vue'
 import { usePage } from '@inertiajs/vue3'
-import Layout from '../../Layouts/layout.vue'
+import Layout from "@/Layouts/layout.vue";
 
 const page = usePage()
 const user = computed(() => page.props.auth.user)
