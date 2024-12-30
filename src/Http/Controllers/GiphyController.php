3595031feb34a947d8d7f00a4c0c86873da52d8d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-30 18:01:47 +0800

    ✨ feat(GiphyController): Implement Giphy trending and search endpoints.

--- src/Http/Controllers/GiphyController.php
+++ src/Http/Controllers/GiphyController.php
@@ -17,15 +17,27 @@ class GiphyController
                     'rating' => 'g',
                     'bundle' => 'messaging_non_clips',
                 ])->get('/gifs/trending');
+
+            return response()->json($reps);
         }
     }
 
     public function search(Request $request)
     {
-        $reps = Http::giphy()
-            ->withQueryParameters([
-                'api_key' => '',
-                'q' => $request->query('q'),
-            ])->get('/gifs/search');
+        if ($request->wantsJson()) {
+            $reps = Http::giphy()
+                ->withQueryParameters([
+                    'api_key' => config('ninshiki.giphy.token'),
+                    'limit' => config('ninshiki.giphy.result.limit') ?? 30,
+                    'rating' => 'g',
+                    'offset' => $request->query('offset') ?? 0,
+                    'bundle' => 'messaging_non_clips',
+                    'lang' => 'en',
+                    'q' => $request->query('q'),
+                ])->get('/gifs/search');
+
+            return response()->json($reps);
+        }
+
     }
 }
