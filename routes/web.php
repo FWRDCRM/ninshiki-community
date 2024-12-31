b65af0ce86c4beaee6868af6b02a37c1ffea16e1
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-31 17:39:31 +0800

    ✨ feat(routes/web.php): Add routes for employee listing and update existing routes.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use MarJose123\Ninshiki\Http\Controllers\AuthenticationController;
+use MarJose123\Ninshiki\Http\Controllers\EmployeesController;
 use MarJose123\Ninshiki\Http\Controllers\FeedsController;
 use MarJose123\Ninshiki\Http\Controllers\GiphyController;
 
@@ -16,8 +17,8 @@ Route::middleware(config('ninshiki.middleware'))
         Route::middleware(config('ninshiki.guestMiddleware'))
             ->group(function () {
                 Route::get('/', [AuthenticationController::class, 'index'])->name('login.page');
-                Route::get('/provider/zoho', [AuthenticationController::class, 'requestForProviderLogin'])->name('login.requestProvider');
-                Route::get('/callback/provider/zoho', [AuthenticationController::class, 'callbackForProviderLogin'])->name('login.callback');
+                Route::get('provider/zoho', [AuthenticationController::class, 'requestForProviderLogin'])->name('login.requestProvider');
+                Route::get('callback/provider/zoho', [AuthenticationController::class, 'callbackForProviderLogin'])->name('login.callback');
             });
 
         /**
@@ -28,12 +29,15 @@ Route::middleware(config('ninshiki.middleware'))
         Route::middleware(config('ninshiki.authMiddleware'))
             ->group(function () {
                 // logout
-                Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+                Route::post('logout', [AuthenticationController::class, 'logout'])->name('logout');
                 //post
-                Route::get('/feed', [FeedsController::class, 'index'])->name('feed');
-                Route::patch('/feed/{id}', [FeedsController::class, 'likeUnlike'])->name('feeds.like-unlike');
+                Route::get('feed', [FeedsController::class, 'index'])->name('feed');
+                Route::patch('feed/{id}', [FeedsController::class, 'likeUnlike'])->name('feeds.like-unlike');
+                //users
+                Route::get('employees', [EmployeesController::class, 'getAllEmployees'])->name('employees');
                 //giphy
-                Route::get('/gif', [GiphyController::class, 'trending'])->name('gif.trending');
-                Route::get('/gif/search', [GiphyController::class, 'search'])->name('gif.search');
+                Route::get('gif', [GiphyController::class, 'trending'])->name('gif.trending');
+                Route::get('gif/search', [GiphyController::class, 'search'])->name('gif.search');
+
             });
     });
