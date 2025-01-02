1d8232af8bfa0ce9831214ee4de17a2f94e4bfec
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-02 23:40:42 +0800

    ✨ feat(src/Http/Controllers/FeedsController.php):  Conditionally add the `gif_url` field when creating a feed.

--- src/Http/Controllers/FeedsController.php
+++ src/Http/Controllers/FeedsController.php
@@ -66,7 +66,7 @@ class FeedsController
                 'post_content' => $request->post_content,
                 'amount' => $request->points,
                 'attachment_type' => $request->attachment_type,
-                'gif_url' => $request->gif_url,
+                ...($request->has('gif_url') ? ['gif_url' => $request->gif_url] : []),
                 'type' => 'user',
                 'recipient_id' => $request->recipient_id,
             ]);
