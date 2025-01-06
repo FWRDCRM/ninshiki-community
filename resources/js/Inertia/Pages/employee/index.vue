911f926f1d504f93a78ad989981212b85542ca7d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-06 15:32:43 +0800

    ✨ feat(`resources/js/Inertia/Pages/employee/index.vue`): Add employee index page.

---
+++ resources/js/Inertia/Pages/employee/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Layout from "@/Layouts/layout.vue";
+import StatisticCard from "@/Components/Employee/Detailed/StatisticCard.vue";
+
+defineOptions({layout: Layout})
+
+
+
+const {employees } = defineProps({ employees: Object });
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
