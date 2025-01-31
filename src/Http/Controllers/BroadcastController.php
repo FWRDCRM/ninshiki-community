d30067cbefc64ca1579435639cfed1c398db16be
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-31 08:57:17 +0800

    ✨ feat(src/Http/Controllers/BroadcastController.php): Inject Request object dependency and use it for retrieving session token and request data.

--- src/Http/Controllers/BroadcastController.php
+++ src/Http/Controllers/BroadcastController.php
@@ -3,6 +3,7 @@
 namespace MarJose123\Ninshiki\Http\Controllers;
 
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class BroadcastController
@@ -10,12 +11,12 @@ class BroadcastController
     /**
      * @throws ConnectionException
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $response = Http::ninshiki()
-            ->withToken(\request()->session()->get('token'))
+            ->withToken($request->session()->get('token'))
             ->post('/broadcasting/auth', [
-                ...\request()->all(),
+                ...$request->all(),
             ]);
 
         return response()->json($response->json(), $response->status());
