1c12d2f3d542124963adeee80c59b8b557a36a26
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-26 10:35:14 +0800

    ♻️ refactor(`resources/js/Inertia/Pages/employee/index.vue`): Improve code style and add error handling to gift sending functionality.  Enhance user experience with improved modal and input handling.

--- resources/js/Inertia/Pages/employee/index.vue
+++ resources/js/Inertia/Pages/employee/index.vue
@@ -1,121 +1,158 @@
 <script setup>
-import Layout from "@/Layouts/layout.vue";
-import {computed, ref, watch} from "vue";
-import {useForm, usePage} from "@inertiajs/vue3";
+import Layout from '@/Layouts/layout.vue';
+import { useForm, usePage } from '@inertiajs/vue3';
 import Select from 'primevue/select';
+import { computed, ref, watch } from 'vue';
 
-defineOptions({layout: Layout})
-
+defineOptions({ layout: Layout });
 
 const page = usePage();
-const user = computed(() => page.props.auth.user)
+const user = computed(() => page.props.auth.user);
 const employees = ref(page.props.employees);
 const isGiftFeatureEnabled = computed(() => page.props.gift_feature_enable);
-const giftType = ref(page.props.gift_type)
+const giftType = ref(page.props.gift_type);
 const exchange_rate = computed(() => page.props.gift_exchange_rate);
 
+const isGiftModalOpen = ref(false);
 
-const isGiftModalOpen = ref(false)
-
-const toggleDialog = (employeeId) => {
-    isGiftModalOpen.value = !!isGiftModalOpen
-    giftForm.receiver = employeeId
-}
+const toggleDialog = (employeeId, name) => {
+    isGiftModalOpen.value = !!isGiftModalOpen;
+    giftForm.receiver.id = employeeId;
+    giftForm.receiver.name = name;
+};
 
 const giftForm = useForm({
     type: undefined,
     amount: undefined,
-    receiver: undefined,
-})
+    receiver: {
+        id: undefined,
+        name: undefined,
+    },
+});
 
 const onSubmit = () => {
-    giftForm.processing = true
+    giftForm.processing = true;
     // clear the error state
-    giftForm.clearErrors()
-    if(giftForm.type === 'shop'){
-        giftForm.setError('type', 'Sending Gift via Shop is not available at this moment.')
+    giftForm.clearErrors();
+    if (giftForm.type === 'shop') {
+        giftForm.setError('type', 'Sending Gift via Shop is not available at this moment.');
+        giftForm.processing = false;
         return;
     }
 
-    NinshikiApp.request().post(route('gift.send'),{
-        type: giftForm.type,
-        amount: giftForm.amount,
-        receiver: giftForm.receiver
-    }).then((resp) => {
-        giftForm.processing = false
-        console.info(resp)
-    }).catch(resp => {
-        giftForm.processing = false
-        console.error(resp)
-    })
+    NinshikiApp.request()
+        .post(route('gift.send'), {
+            type: giftForm.type,
+            amount: giftForm.amount,
+            receiver: giftForm.receiver?.id,
+        })
+        .then((resp) => {
+            giftForm.processing = false;
+            console.info(resp);
+        })
+        .catch((resp) => {
+            giftForm.processing = false;
+            if (resp.status === 422) {
+                const amountError = resp.response.data.error.errors.amount[0];
+                if (amountError) {
+                    giftForm.setError('amount', amountError);
+                }
+            }
+            console.error(resp);
+        });
+};
 
+watch(
+    () => giftForm.type,
+    (val) => {
+        if (giftForm.type !== val?.type) {
+            giftForm.clearErrors();
+        }
+    },
+);
 
-}
-
-watch(() => giftForm.type, (val) => {
-    if(giftForm.type !== val?.type){
-        giftForm.clearErrors()
-    }
-})
-
-watch(() => giftForm.amount, (val) => {
-    if(giftForm.amount !== val?.amount){
-        giftForm.clearErrors()
-    }
-})
-
-
+watch(
+    () => giftForm.amount,
+    (val) => {
+        if (giftForm.amount !== val?.amount) {
+            giftForm.clearErrors();
+        }
+    },
+);
 </script>
 
 <template>
-    <div class="max-w-screen-lg mx-auto mb-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 w-full">
-        <Dialog v-model:visible="isGiftModalOpen" :block-scroll="true" :closable="!giftForm.processing" position="center" modal header="Send Gift" :style="{ width: '20rem' }">
-            <Message severity="info" class="w-full my-4"><i class="text-xs text-balance">Current exchange rate for sending gift coins: {{ exchange_rate }}</i></Message>
-            <div class="flex flex-col gap-4 mb-4">
-                <label for="gift_type" class="font-semibold w-auto">Select Type of Gift</label>
-                <Select v-model="giftForm.type" :options="giftType" placeholder="Select type of gift" class="w-ful"
-                        :pt="{
-                   optionLabel: {
-                       class: 'capitalize'
-                   },
-                   label: {
-                     class: 'capitalize'
-                   }
-                }" required/>
+    <div class="mx-auto mb-4 grid w-full max-w-screen-lg grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+        <Dialog
+            v-model:visible="isGiftModalOpen"
+            :block-scroll="true"
+            :closable="!giftForm.processing"
+            position="top"
+            modal
+            header="Send Gift"
+            :style="{ width: '20rem' }"
+        >
+            <Message severity="info" class="my-4 w-full"
+                ><i class="text-balance text-xs">Current exchange rate for sending gift coins: {{ exchange_rate }}</i></Message
+            >
+            <div class="mb-8 flex flex-col gap-4">
+                <label for="amount" class="w-24 font-semibold">Recipient</label>
+                <InputText class="flex-auto" autocomplete="off" v-model="giftForm.receiver.name" disabled />
+            </div>
+            <div class="mb-4 flex flex-col gap-4">
+                <label for="gift_type" class="w-auto font-semibold">Select Type of Gift</label>
+                <Select
+                    v-model="giftForm.type"
+                    :options="giftType"
+                    placeholder="Select type of gift"
+                    class="w-ful"
+                    :pt="{
+                        optionLabel: {
+                            class: 'capitalize',
+                        },
+                        label: {
+                            class: 'capitalize',
+                        },
+                    }"
+                    required
+                />
                 <Message v-if="giftForm.errors?.type" severity="error" size="small" variant="simple">
                     {{ giftForm.errors?.type }}
                 </Message>
             </div>
-            <div v-if="giftForm?.type === 'coins' " class="flex flex-col gap-4 mb-8">
-                <label for="amount" class="font-semibold w-24">Amount</label>
-                <InputNumber v-model="giftForm.amount" id="amount" :min="0" class="flex-auto" autocomplete="off"
-                             :useGrouping="false" required/>
+            <div v-if="giftForm?.type === 'coins'" class="mb-8 flex flex-col gap-4">
+                <label for="amount" class="w-24 font-semibold">Amount</label>
+                <InputNumber v-model="giftForm.amount" id="amount" :min="0" class="flex-auto" autocomplete="off" :useGrouping="false" required />
                 <Message v-if="giftForm.errors?.amount" severity="error" size="small" variant="simple">
                     {{ giftForm.errors?.amount }}
                 </Message>
             </div>
             <div class="flex justify-end gap-2">
                 <Button type="button" label="Cancel" severity="secondary" :disabled="giftForm.processing" @click="isGiftModalOpen = false"></Button>
-                <Button type="button" icon="pi pi-send" label="Send" :disabled="giftForm.processing" :loading="giftForm.processing" @click.prevent="onSubmit"></Button>
+                <Button
+                    type="button"
+                    icon="pi pi-send"
+                    label="Send"
+                    :disabled="giftForm.processing"
+                    :loading="giftForm.processing"
+                    @click.prevent="onSubmit"
+                ></Button>
             </div>
         </Dialog>
 
         <div v-for="employee in employees" :key="employee.id" class="flex w-full">
-            <Card class="min-w-48 w-full">
+            <Card class="w-full min-w-48">
                 <template #header>
-                    <div class="flex flex-wrap items-center justify-center mt-4">
-                        <Avatar :alt="employee.name" :image="employee.avatar ?? $ninshiki.uiAvatar(employee.name)"
-                                shape="circle" size="xlarge"/>
+                    <div class="mt-4 flex flex-wrap items-center justify-center">
+                        <Avatar :alt="employee.name" :image="employee.avatar ?? $ninshiki.uiAvatar(employee.name)" shape="circle" size="xlarge" />
                     </div>
                 </template>
                 <template #title>
                     <div class="text-balance text-center">{{ employee.name }}</div>
                 </template>
                 <template #subtitle>
-                    <div class="text-center space-x-3">
-                        <div class="italic">
-                            @{{ employee.username }}
-                        </div>
+                    <div class="space-x-3 text-center">
+                        <div class="italic">@{{ employee.username }}</div>
                         <div>
                             {{ employee.email }}
                         </div>
@@ -123,9 +160,15 @@ watch(() => giftForm.amount, (val) => {
                 </template>
                 <template #footer>
                     <div class="text-center">
-                        <Button v-if="isGiftFeatureEnabled && user?.id !== employee.id" icon="pi pi-gift" variant="text"
-                                rounded aria-label="Gift"
-                                v-tooltip.bottom="'Send Gift'" @click="toggleDialog(employee.id)"/>
+                        <Button
+                            v-if="isGiftFeatureEnabled && user?.id !== employee.id"
+                            icon="pi pi-gift"
+                            variant="text"
+                            rounded
+                            aria-label="Gift"
+                            v-tooltip.bottom="'Send Gift'"
+                            @click="toggleDialog(employee.id, employee.name)"
+                        />
                     </div>
                 </template>
             </Card>
@@ -133,6 +176,4 @@ watch(() => giftForm.amount, (val) => {
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
