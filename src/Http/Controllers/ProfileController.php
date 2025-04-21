2294f5b5451c3a0b3f762aa9ea20f65b406a3a21
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-04-21 09:32:11 +0800

    ✨ feat(profile): Implement logout other devices feature and fetch data concurrently.

--- src/Http/Controllers/ProfileController.php
+++ src/Http/Controllers/ProfileController.php
@@ -3,22 +3,44 @@
 namespace MarJose123\Ninshiki\Http\Controllers;
 
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
 
 class ProfileController
 {
+    public function index()
+    {
+        $resp = Http::pool(fn (Pool $pool) => [
+            $pool->as('devices')
+                ->ninshiki()
+                ->withToken(\request()->session()->get('token'))
+                ->get(config('ninshiki.api_version').'/sessions'),
+            $pool->as('permissions')
+                ->ninshiki()
+                ->withToken(\request()->session()->get('token'))
+                ->get(config('ninshiki.api_version').'/profile/permissions'),
+        ]);
+
+        return Inertia::render('profile/index', [
+            'logout_other_devices' => [
+                'required_password' => in_array('access panel', $resp['permissions']->json()),
+            ],
+            'devices' => $resp['devices']->json(),
+        ]);
+    }
+
     /**
+     * Logout other devices
+     *
      * @throws ConnectionException
      */
-    public function index()
+    public function logoutOtherDevices()
     {
         $resp = Http::ninshiki()
             ->withToken(\request()->session()->get('token'))
-            ->get(config('ninshiki.api_version').'/sessions');
+            ->post(config('ninshiki.api_version').'/sessions/logout/devices');
 
-        return Inertia::render('profile/index', [
-            'devices' => $resp->json(),
-        ]);
+        return to_route('profile');
     }
 }
