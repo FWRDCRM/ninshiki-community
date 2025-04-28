2587e6bdd30b897a944f57bccd616255b4d849ad
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-04-28 17:07:54 +0800

    💄 feat(StorePage): Improve product card UI and layout presentation. Display stock and price details more clearly.

--- resources/js/Inertia/Pages/store/index.vue
+++ resources/js/Inertia/Pages/store/index.vue
@@ -10,41 +10,55 @@ const products = ref(page.props.products);
 </script>
 
 <template>
-    <div v-for="product in products" :key="product.id" class="flex w-full">
-        <Card class="min-[120px] w-full">
-            <template #header>
-                <div class="mt-4 flex flex-wrap items-center justify-center">
-                    <Image
-                        :alt="product.name"
-                        src="https://d1r3vc4fck3z1b.cloudfront.net/images/1643763414_web_variance_hKVR7MB7.jpeg"
-                        width="180"
-                        height="210"
-                    />
-                </div>
-            </template>
-            <template #title>
-                <div class="text-balance text-center">{{ product.product.name }}</div>
-            </template>
-            <template #subtitle>
-                <div class="flex flex-col gap-3">
-                    <div>
-                        <Chip :label="`${product.product.stock} Available Stock`" v-if="product.product.stock > 0" class="bg-green-500 text-white" />
-                        <Chip label="Out of Stock" v-else class="bg-red-600 text-white" />
+    <div class="min-w-[1000px] max-w-full">
+        <div v-for="product in products" :key="product.id" class="flex max-w-[250px]">
+            <Card class="w-full">
+                <template #header>
+                    <div class="mt-4 flex flex-wrap items-center justify-center">
+                        <Image
+                            :alt="product.name"
+                            src="https://d1r3vc4fck3z1b.cloudfront.net/images/1643763414_web_variance_hKVR7MB7.jpeg"
+                            width="180"
+                            height="210"
+                        />
                     </div>
-                    <div class="space-x-3 whitespace-pre text-center">
-                        <div v-if="product.product.description !== product.product.name">
-                            {{ product.product.description }}
+                </template>
+                <template #title>
+                    <div class="text-balance text-center">{{ product.product.name }}</div>
+                </template>
+                <template #subtitle>
+                    <div class="flex flex-col gap-3">
+                        <div class="text-sm">
+                            <Chip label="Out of Stock" v-if="product.product.stock === 0" class="bg-red-600 text-white" />
+                        </div>
+                        <div class="space-x-3 whitespace-pre text-center">
+                            <div v-if="product.product.description !== product.product.name">
+                                {{ product.product.description }}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </template>
-            <template #footer>
-                <div class="flex flex-row-reverse gap-2">
-                    <Button label="Purchase" severity="secondary" />
-                    <Button icon="pi pi-heart" severity="help" variant="text" rounded aria-label="Favorite" v-tooltip.bottom="'Add to Wishlist'" />
-                </div>
-            </template>
-        </Card>
+                </template>
+                <template #content>
+                    <div class="flex flex-row flex-wrap justify-between text-sm">
+                        <p class="text-sm">Stock: {{ product.product.stock }}</p>
+                        <p class="text-sm">Price: {{ product.product.price }}</p>
+                    </div>
+                </template>
+                <template #footer>
+                    <div class="flex flex-row-reverse gap-2 pt-5">
+                        <Button label="Purchase" severity="primary" />
+                        <Button
+                            icon="pi pi-heart"
+                            severity="help"
+                            variant="text"
+                            rounded
+                            aria-label="Favorite"
+                            v-tooltip.bottom="'Add to Wishlist'"
+                        />
+                    </div>
+                </template>
+            </Card>
+        </div>
     </div>
 </template>
 
